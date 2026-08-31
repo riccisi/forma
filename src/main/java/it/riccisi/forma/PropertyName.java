b2f0e1b6b7411d6ad1652358f0e5b7100bed7de4
riccisi <simone.ricciardi@gmail.com>
2026-08-31 15:33:33 +0200

    Introduce representation property names for mapping spike

---
+++ src/main/java/it/riccisi/forma/PropertyName.java
@@ -0,0 +1,10 @@
+package it.riccisi.forma;
+
+/**
+ * Name of a property inside a concrete data representation.
+ *
+ * <p>A property name belongs to the representation side of Forma and is distinct
+ * from the semantic identity expressed by {@link AttributeName}.
+ */
+public interface PropertyName {
+}
