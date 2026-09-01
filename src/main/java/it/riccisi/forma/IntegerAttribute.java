127d8e4cd3ee59319f22c5721253d460e25b4db2
riccisi <simone.ricciardi@gmail.com>
2026-09-01 10:48:58 +0200

    Add integer semantic attribute

---
+++ src/main/java/it/riccisi/forma/IntegerAttribute.java
@@ -0,0 +1,34 @@
+package it.riccisi.forma;
+
+import java.math.BigDecimal;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Semantic integer interpreted from a numeric property value.
+ */
+@RequiredArgsConstructor
+public final class IntegerAttribute extends NumberAttribute<Integer> {
+
+    @NonNull private final AttributeName<Integer> name;
+
+    @Override
+    public AttributeName<Integer> name() {
+        return this.name;
+    }
+
+    @Override
+    protected ModelAttribute<Integer> bind(final Number value) {
+        try {
+            return new BoundAttribute<>(
+                this.name,
+                new BigDecimal(value.toString()).intValueExact()
+            );
+        } catch (final ArithmeticException | NumberFormatException err) {
+            throw new IllegalArgumentException(
+                "The represented number cannot be interpreted as an integer",
+                err
+            );
+        }
+    }
+}
